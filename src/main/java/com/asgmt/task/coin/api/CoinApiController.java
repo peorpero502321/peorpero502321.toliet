a820bf348a8fd6a1b6892e9b6228f9799cf2da22
peropero502321 <peropero50@naver.com>
2023-02-24 10:30:45 +0900

    API controller CORS 설정 적용

--- src/main/java/com/asgmt/task/coin/api/CoinApiController.java
+++ src/main/java/com/asgmt/task/coin/api/CoinApiController.java
@@ -3,6 +3,7 @@ package com.asgmt.task.coin.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import com.asgmt.task.coin.service.CoinService;
 import com.asgmt.task.coin.vo.SuggestionCoinPVo;
 import com.asgmt.task.coin.vo.SuggestionCoinRVo;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class CoinApiController {
 
